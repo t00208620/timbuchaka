babc37677857e0e8cd2435e116d5e0c0dab166e5
t00208620 <55188878+t00208620@users.noreply.github.com>
2021-01-13 20:22:18 +0000

    wed eve

--- src/OOP/Dog.java
+++ src/OOP/Dog.java
@@ -16,4 +16,37 @@ public class Dog extends Animal {
         this.teeth = teeth;
         this.coat = coat;
     }
+
+    private void chew() {
+        System.out.println("Dog.chew() called");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat() called");
+        chew();
+        super.eat();
+    }
+
+    public void walk() {
+        System.out.println("Dog.walk() called");
+        move(5);
+    }
+
+
+    public void run() {
+        System.out.println("Dog.run() called");
+        move(10);
+    }
+
+    private void moveLegs(int speed){
+        System.out.println("Dog.moveLegs() called");
+    }
+
+    @Override
+  public void move(int speed) {
+        System.out.println("Dog.move() called");
+       moveLegs(speed);
+        super.move(speed);
+    }
 }
